789a51eb8ff31dd880bf6f6fb94f1deaf11b8574
Andrea Li <132218960+qndreali@users.noreply.github.com>
2024-11-18 20:20:43 +0800

    refacctor(rotations): combine random rotation methods

--- main.py
+++ main.py
@@ -5,14 +5,10 @@ from scipy.stats import special_ortho_group
 
 class Rotations:
     @staticmethod
-    def generate_random_rotation(dim=3):
-        return special_ortho_group.rvs(dim)
-    
-    @classmethod
-    def random_rotations(cls, num_matrices, dim=3):
+    def random_rotations(num_matrices, dim=3):
         if num_matrices < 1:
             raise ValueError("Number of matrices must be at least 1.")
-        return [cls.generate_random_rotation(dim) for _ in range(num_matrices)]
+        return [special_ortho_group.rvs(dim) for _ in range(num_matrices)]
     
     @staticmethod
     def optimal_rotations():
