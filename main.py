03aa140a248a5741e6f92d57c0cee843f3ae31cf
Andrea Li <132218960+qndreali@users.noreply.github.com>
2024-11-18 19:39:39 +0800

    feat(rotations): add method to generate multiple random rotation matrices

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ class Rotations:
     @staticmethod
     def generate_random_rotation(dim=3):
         return special_ortho_group.rvs(dim)
-
+    
+    def random_rotations(cls, num_matrices, dim=3):
+        if num_matrices < 1:
+            raise ValueError("Number of matrices must be at least 1.")
+        
+        return [cls.generate_random_rotation(dim) for _ in range(num_matrices)]
