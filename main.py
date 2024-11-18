dde19550b3afda8d8133216c6f5244cb1dbf460f
Andrea Li <132218960+qndreali@users.noreply.github.com>
2024-11-18 19:38:49 +0800

    feat(rotations): add static decorator

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.spatial.transform import special_ortho_group
 
 class Rotations:
+    @staticmethod
     def generate_random_rotation(dim=3):
         return special_ortho_group.rvs(dim)
 
