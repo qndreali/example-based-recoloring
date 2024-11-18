5213791636e7e21fa81818ffb0d4d7b1bce57eed
Andrea Li <132218960+qndreali@users.noreply.github.com>
2024-11-18 19:49:14 +0800

    feat(color-transfer): integrate output transformation in pdf_transfer

--- main.py
+++ main.py
@@ -48,4 +48,10 @@ class ColorTransfer:
         h, w, c = img_input.shape
         input_pixels = img_input.reshape(-1, c).T / 255.0
         reference_pixels = img_reference.reshape(-1, c).T / 255.0
+        output_pixels = self._transfer_pixels(input_pixels, reference_pixels)
+        
+        output_pixels = np.clip(output_pixels * 255.0, 0, 255).astype("uint8")
+        output_img = output_pixels.T.reshape(h, w, c)
+
+        return output_img
  
