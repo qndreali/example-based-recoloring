3ac721aa2acfb45c86e614a5ccf9ae11ae95c28e
Andrea Li <132218960+qndreali@users.noreply.github.com>
2024-11-18 19:38:31 +0800

    feat(rotations): add method to generate a single random rotation matrix

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+from scipy.spatial.transform import special_ortho_group
+
+class Rotations:
+    def generate_random_rotation(dim=3):
+        return special_ortho_group.rvs(dim)
+
