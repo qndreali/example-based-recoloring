a9e6b6b1201ce096d80a77c21900e5f87f06f562
Andrea Li <132218960+qndreali@users.noreply.github.com>
2024-11-18 19:44:55 +0800

    feat(color-transfer): initialize color transfer class

--- main.py
+++ main.py
@@ -35,4 +35,7 @@ class Rotations:
         return predefined_matrices
     
 class ColorTransfer:
-    pass
+    def __init__(self, eps=1e-6, matrix_count=6, color_channels=3):
+        self.eps = eps
+        self.rotation_matrices = (Rotations.optimal_rotations() if color_channels == 3 
+                                  else Rotations.random_rotations(matrix_count, color_channels))
